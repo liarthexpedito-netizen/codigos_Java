4835ad8f6403ab38cf7864764b209ee78edc5466
Expedito <liarthexpedito@gmail.com>
2026-09-21 15:10:53 -0300

    Confirmando código Java

---
+++ orientacao_objetos/Problema_sem_OO.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Problema_sem_OO {
+
+    public static void main(String[] args) {
+        
+    Scanner sc = new Scanner(System.in);
+
+    
+
+    sc.close();
+    }
+
+}
